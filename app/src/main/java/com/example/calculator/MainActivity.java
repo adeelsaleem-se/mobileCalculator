75d97b82af2aa80da0c3af70b8c51e5886a4f905
Adeel Saleem <62308816+adeelsaleem-se@users.noreply.github.com>
2021-10-23 17:13:40 +0500

    onClick() prototypes added

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,71 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void one(View view){
+    }
+
+    public void clearall(View view) {
+    }
+
+    public void clear(View view) {
+    }
+
+    public void sign(View view) {
+    }
+
+    public void backspace(View view) {
+    }
+
+    public void percent(View view) {
+    }
+
+    public void inverse(View view) {
+    }
+
+    public void square(View view) {
+    }
+
+    public void multiply(View view) {
+    }
+
+    public void seven(View view) {
+    }
+
+    public void eight(View view) {
+    }
+
+    public void nine(View view) {
+    }
+
+    public void divide(View view) {
+    }
+
+    public void two(View view) {
+    }
+
+    public void three(View view) {
+    }
+
+    public void plus(View view) {
+    }
+
+    public void four(View view) {
+    }
+
+    public void five(View view) {
+    }
+
+    public void six(View view) {
+    }
+
+    public void minus(View view) {
+    }
+
+    public void zero(View view) {
+    }
+
+    public void answer(View view) {
     }
 }
