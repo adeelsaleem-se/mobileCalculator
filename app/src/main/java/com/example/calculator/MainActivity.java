49edde043cac8c4d2ca82fe43d16924fdd887e5a
Adeel Saleem <62308816+adeelsaleem-se@users.noreply.github.com>
2021-10-23 19:50:25 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -17,10 +17,15 @@ public class MainActivity extends AppCompatActivity {
     public void one(View view){
         EditText field2  = (EditText) findViewById(R.id.field2);
         String temp = field2.getText().toString();
-        field2.setText(temp + "1");
+        if(temp.length() < 8)
+            field2.setText(temp + "1");
     }
 
     public void clearall(View view) {
+        EditText field1  = (EditText) findViewById(R.id.field1);
+        EditText field2  = (EditText) findViewById(R.id.field2);
+
+
     }
 
     public void clear(View view) {
@@ -45,41 +50,92 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void seven(View view) {
+        EditText field2  = (EditText) findViewById(R.id.field2);
+        String temp = field2.getText().toString();
+        if(temp.length() < 8)
+            field2.setText(temp + "7");
     }
 
     public void eight(View view) {
+        EditText field2  = (EditText) findViewById(R.id.field2);
+        String temp = field2.getText().toString();
+        if(temp.length() < 8)
+            field2.setText(temp + "8");
     }
 
     public void nine(View view) {
+        EditText field2  = (EditText) findViewById(R.id.field2);
+        String temp = field2.getText().toString();
+        if(temp.length() < 8)
+            field2.setText(temp + "9");
     }
 
     public void divide(View view) {
     }
 
     public void two(View view) {
+        EditText field2  = (EditText) findViewById(R.id.field2);
+        String temp = field2.getText().toString();
+        if(temp.length() < 8)
+            field2.setText(temp + "2");
     }
 
     public void three(View view) {
+        EditText field2  = (EditText) findViewById(R.id.field2);
+        String temp = field2.getText().toString();
+        if(temp.length() < 8)
+            field2.setText(temp + "3");
     }
 
     public void plus(View view) {
     }
 
     public void four(View view) {
+        EditText field2  = (EditText) findViewById(R.id.field2);
+        String temp = field2.getText().toString();
+        if(temp.length() < 8)
+            field2.setText(temp + "4");
     }
 
     public void five(View view) {
+        EditText field2  = (EditText) findViewById(R.id.field2);
+        String temp = field2.getText().toString();
+        if(temp.length() < 8)
+            field2.setText(temp + "5");
     }
 
     public void six(View view) {
+        EditText field2  = (EditText) findViewById(R.id.field2);
+        String temp = field2.getText().toString();
+        if(temp.length() < 8)
+            field2.setText(temp + "6");
     }
 
     public void minus(View view) {
+        EditText field1  = (EditText) findViewById(R.id.field1);
+        EditText field2  = (EditText) findViewById(R.id.field2);
+
+        String temp = field2.getText().toString();
+        field1.setText(temp + "-");
+        field2.setText("");
     }
 
     public void zero(View view) {
+        EditText field2  = (EditText) findViewById(R.id.field2);
+        String temp = field2.getText().toString();
+        if(temp.length() < 8)
+            field2.setText(temp + "0");
     }
 
     public void answer(View view) {
+        EditText field1  = (EditText) findViewById(R.id.field1);
+        EditText field2  = (EditText) findViewById(R.id.field2);
+
+        String s = field1.getText().toString();
+        int value1 = Integer.parseInt(s.substring(0,s.length()-1));
+        int value2 = Integer.parseInt(field2.getText().toString());
+
+        field1.setText(field1.getText().toString() + field2.getText().toString() + " =");
+        field2.setText("Answer:" + String.valueOf(value1 - value2));
     }
 }
